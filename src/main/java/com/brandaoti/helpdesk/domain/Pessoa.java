c06393c2f066368e4e6a99ddb4fd0cdfd1e9e724
Henrique Brandão <hb06ars@hotmail.com>
2022-07-12 08:12:08 -0300

    Anotacoes JPA
    Anotacoes JPA

--- src/main/java/com/brandaoti/helpdesk/domain/Pessoa.java
+++ src/main/java/com/brandaoti/helpdesk/domain/Pessoa.java
@@ -1,23 +1,47 @@
 package com.brandaoti.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import com.brandaoti.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 // Abstract porque nao sera usado essa classe diretamente, sera usado ou tecnico ou cliente
 // protect porque todos os tecnicos e clientes que herdarem a pessoa poderao acessar os dados.
 
-public abstract class Pessoa {
+@Entity
+public abstract class Pessoa implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String nome;
+	
+	@Column (unique = true)
 	protected String cpf;
+	
+	@Column (unique = true)
 	protected String email;
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name="PERFIS")
 	protected Set<Integer> perfis = new HashSet<>(); //Não permite 2 valores iguais na lista.
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 	
 	
