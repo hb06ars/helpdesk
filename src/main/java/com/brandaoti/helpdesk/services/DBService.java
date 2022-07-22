e70347ad1e04468bb4e5eec9758a74428428e316
Henrique Brandão <hb06ars@hotmail.com>
2022-07-22 18:31:55 -0300

    ok
    ok

--- src/main/java/com/brandaoti/helpdesk/services/DBService.java
+++ src/main/java/com/brandaoti/helpdesk/services/DBService.java
@@ -3,6 +3,7 @@ package com.brandaoti.helpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.brandaoti.helpdesk.domain.Chamado;
@@ -12,30 +13,28 @@ import com.brandaoti.helpdesk.domain.enums.Perfil;
 import com.brandaoti.helpdesk.domain.enums.Prioridade;
 import com.brandaoti.helpdesk.domain.enums.Status;
 import com.brandaoti.helpdesk.repositories.ChamadoRepository;
-import com.brandaoti.helpdesk.repositories.ClienteRepository;
-import com.brandaoti.helpdesk.repositories.TecnicoRepository;
+import com.brandaoti.helpdesk.repositories.PessoaRepository;
 
 @Service //Essa classe serve para injeção de dependencias, o spring cria, destroi, etc
 public class DBService {
 	
 	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
+	private PessoaRepository pessoaRepository;
 	@Autowired
 	private ChamadoRepository chamadoRepository;
-	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	
 	public void instanciaDB() {
-		Tecnico t1 = new Tecnico(null, "Henrique Brandão", "98062783057", "teste@teste.com", "123");
+		Tecnico t1 = new Tecnico(null, "Henrique Brandão", "98062783057", "teste@teste.com", encoder.encode("123"));
 		t1.addPerfil(Perfil.TECNICO);
 		
-		Cliente cli1 = new Cliente(null, "Linus", "99333717030", "cliente@cliente.com", "123");
+		Cliente cli1 = new Cliente(null, "Linus", "99333717030", "cliente@cliente.com", encoder.encode("123"));
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", t1, cli1);
 		
-		tecnicoRepository.saveAll(Arrays.asList(t1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
+		pessoaRepository.saveAll(Arrays.asList(t1));
+		pessoaRepository.saveAll(Arrays.asList(cli1));
 		chamadoRepository.saveAll(Arrays.asList(c1));
 		
 	}
