6b47e17924d830709444823c5bb9653631f67934
Henrique Brandão <hb06ars@hotmail.com>
2022-08-01 15:51:24 -0300

    ok
    ok

--- src/main/java/com/brandaoti/helpdesk/repositories/ChamadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brandaoti.helpdesk.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.brandaoti.helpdesk.domain.Chamado;
-
-public interface ChamadoRepository extends JpaRepository<Chamado, Integer>{
-
-}
--- src/main/java/com/brandaoti/helpdesk/repositories/TecnicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brandaoti.helpdesk.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.brandaoti.helpdesk.domain.Tecnico;
-
-public interface TecnicoRepository extends JpaRepository<Tecnico, Integer>{
-
-}
